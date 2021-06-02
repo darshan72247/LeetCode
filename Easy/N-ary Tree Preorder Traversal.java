26c159dd82b0f96322aa6707a560f7e5bfd50b27
Darshan Patel <66130004+darshan72247@users.noreply.github.com>
2021-06-02 09:24:33 -0400

    N-ary Tree Preorder Traversal Solved

---
+++ Easy/N-ary Tree Preorder Traversal.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    private List<Integer> arr = new ArrayList<Integer>();
+    public List<Integer> preorder(Node root) {
+        if(root == null){
+            return arr;
+        }
+        arr.add(root.val);
+        for(int i=0;i<root.children.size();i++){
+            if(root.children.get(i) != null)
+                preorder(root.children.get(i));
+            
+        }
+        
+        return arr;
+    }
+    
+}
